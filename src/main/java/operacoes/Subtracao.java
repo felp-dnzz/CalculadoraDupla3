5a559d4c06f30431141143cf42c37a37f7930ae4
Felipe Diniz <felipedinizprogamer@gmail.com>
2026-06-17 14:15:47 -0300

    feat: implementa a função de subtração

---
+++ src/main/java/operacoes/Subtracao.java
@@ -0,0 +1,16 @@
+package operacoes;
+
+import java.util.Scanner;
+
+public class Subtracao {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        int y;
+        System.out.println("De o primeiro valor: ");
+        x = scanner.nextInt();
+        System.out.println("De o segundo valor: ");
+        y = scanner.nextInt();
+        System.out.println("o resultado é: " + (x - y));
+    }
+}
